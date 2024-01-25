c2cf887ba6608b5326d5a5f81c30c07e3135ea51
Dikshashukla1 <106049102+Dikshashukla1@users.noreply.github.com>
2024-01-26 00:11:48 +0530

    Update activate.fish

--- venv/Scripts/activate.fish
+++ venv/Scripts/activate.fish
@@ -57,7 +57,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV 'C:\Users\prave\PycharmProjects\Gayathri-Balachandran-Project-3_Python-for-DS\venv'
+set -gx VIRTUAL_ENV 'C:\Users\prave\PycharmProjects\Project-3_Python-for-DS\venv'
 
 # https://github.com/fish-shell/fish-shell/issues/436 altered PATH handling
 if test (echo $FISH_VERSION | head -c 1) -lt 3
